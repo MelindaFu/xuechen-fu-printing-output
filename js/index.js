93e58ae90d5572086f424867b4aea4745c9cf015
MelindaFu <35805422+MelindaFu@users.noreply.github.com>
2021-01-24 21:37:24 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,33 @@
+//Task 1
+var a = 3;
+var b = 5;
+var c;
+alert( 
+    "var a = " + a +"\n" +
+    "var b = " + b +"\n" +
+    "var c;" + "\n" +
+    "----------" + "\n" + 
+    "a + b = " + (a+b) + "\n" +
+    "a - b = " + (a-b) + "\n" +
+    "a * b = " + (a*b) + "\n" +
+    "a / b = " + (a/b) + "\n" +
+    "a % b = " + (a%b) + "\n" +
+    "a += b = " + (a+=b) + "\n" +
+    "a -= b = " + (a-=b) + "\n" +
+    "a *= b = " + (a*=b) + "\n" +
+    "a /= b = " + (a/=b) + "\n" +
+    "a %= b = " + (a%=b) + "\n" +
+    "a == b = " + (a==b) + "\n" +
+    "a != b = " + (a!=b) + "\n" +
+    "a > b = " + (a>b) + "\n" +
+    "a < b = " + (a<b) + "\n" +
+    "!a && c = " + (!a&&c) + "\n" +
+    "!a || !c = " + (!a||!c) + "\n"
+     );
+
+//Task 2
+var first_name = "Xuechen";
+var last_name = "Fu";
+var email = "fu000062@algonquinlive.com";
+var output = "My name is " + first_name + " " + last_name + ". You can contact me at " + email + ".";
+alert(output);
